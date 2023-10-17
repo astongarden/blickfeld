5fa210d0c24203af37b602231a7f40c0c5cd5bee
astongarden <9812choi@naver.com>
2023-10-17 11:44:53 +0900

    first commit

--- build/blickfeld_pointcloud/prefix_override/sitecustomize.py
+++ build/blickfeld_pointcloud/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jeongil/blickfeld_ws/install/blickfeld_pointcloud'
+sys.prefix = sys.exec_prefix = '/home/jeongil/ros_foxy_ws/blickfeld_ws/install/blickfeld_pointcloud'
